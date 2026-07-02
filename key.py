cd532f0bc402dea868195cf49aa17f002bb9e4b9
km2262488 <km2262488@gmail.com>
2026-07-02 08:04:35 +0700

    update

--- key.py
+++ key.py
@@ -3,7 +3,11 @@ import os
 FILE_PATH = os.path.expanduser("~/.termux/termux.properties")
 
 # Layout Keyboard Versi Clean
-CONFIG_CONTENT = 'extra-keys = [["git clone ","&&","CTRL","ALT","HOME","END","DEL"],["UP","LEFT","DOWN","RIGHT","PGUP","PGDN","pip install "],["ls","cd ","pkg ","nano ","python ","clear","exit","ENTER"]]'
+CONFIG_CONTENT = extra-keys = [ \
+['ESC','/','-','HOME','UP','END','PGUP','TAB','|','FORWARD_DEL'], \
+['CTRL','ALT','LEFT','DOWN','RIGHT','PGDN','|','_','=','DEL'], \
+['~','git clone ','&& ','|| ','| ','pip install ','nano ','vim ','exit','ENTER'] \
+]
 
 def install():
     os.makedirs(os.path.dirname(FILE_PATH), exist_ok=True)
@@ -11,7 +15,7 @@ def install():
     with open(FILE_PATH, "w") as f:
         f.write(CONFIG_CONTENT + "\n")
 
-    print("[+] Keyboard Termux berhasil diupdate ke v1.8 Clean")
+    print("[+] Keyboard Termux berhasil diupdate mode Clean")
     print("[+] Layout: git clone | && | pip install | python | nano | ls")
     print("[!] Jalankan 'exit' lalu buka ulang Termux biar kepake")
 
