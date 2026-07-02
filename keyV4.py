9926cce2a54ddde26af87747e9502d9f94521d23
km2262488 <km2262488@gmail.com>
2026-07-02 09:18:39 +0700

    update

--- keyV4.py
+++ keyV4.py
@@ -2,16 +2,14 @@ import os
 
 FILE_PATH = os.path.expanduser("~/.termux/termux.properties")
 
-# Layout 
-CONFIG_CONTENT = 'extra-keys = [["bash ","git clone ","&&","CTRL","node ","HOME","END","DEL","go "],["UP","LEFT","DOWN","RIGHT","PGUP","PGDN","vim ","pip install ","htop"],["ls","cd ","pkg ","nano ","python ","clear","exit","ENTER","rm -rf "],["npm install ","npm run dev ","python manage.py runserver ","docker ","psql ","mysql ","curl ","wget ","exit"]]'
-
+# Layout Keyboard 9x4 Full
+CONFIG_CONTENT = 'extra-keys = [["bash ","git clone ","&&","CTRL","node ","HOME","END","DEL","go "],["UP","LEFT","DOWN","RIGHT","PGUP","PGDN","vim ","pip install ","htop"],["ls","cd ","pkg ","nano ","python ","clear","exit","ENTER","rm -rf "],["npm install ","npm run dev ","python manage.py runserver ","docker ","psql ","mysql ","curl ","wget ","git pull "]]'
 
 def install():
     os.makedirs(os.path.dirname(FILE_PATH), exist_ok=True)
     with open(FILE_PATH, "w") as f:
         f.write(CONFIG_CONTENT + "\n")
-    print("[!] Layout Full ")
-    print("[!] V.4")
+    print("[+] v4.0 FINAL: 9x4 = 36 tombol")
     print("[!] Jalankan: termux-reload-settings")
 
 if __name__ == "__main__":
