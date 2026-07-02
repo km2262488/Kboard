f6c03c4a15083b1f027f50bad2630d51e3b19da9
km2262488 <km2262488@gmail.com>
2026-07-02 09:06:13 +0700

    update

--- keyV3.py
+++ keyV3.py
@@ -1,16 +1,19 @@
+
 import os
 
 FILE_PATH = os.path.expanduser("~/.termux/termux.properties")
 
-# Layout Keyboard V3
-CONFIG_CONTENT = 'extra-keys = [["bash ","git clone ","&&","CTRL","node ","HOME","END","DEL","go "],["UP","LEFT","DOWN","RIGHT","PGUP","PGDN","vim ","pip install ","htop"],["ls","cd ","pkg ","nano ","python ","clear","exit","ENTER",""]]'
+# Layout Keyboard v.3- Full 9x3
+CONFIG_CONTENT = 'extra-keys = [["bash ","git clone ","&&","CTRL","node ","HOME","END","DEL","go "],["UP","LEFT","DOWN","RIGHT","PGUP","PGDN","vim ","pip install ","htop"],["ls","cd ","pkg ","nano ","python ","clear","exit","ENTER","rm -rf "]]'
 
 def install():
     os.makedirs(os.path.dirname(FILE_PATH), exist_ok=True)
     with open(FILE_PATH, "w") as f:
         f.write(CONFIG_CONTENT + "\n")
-    print("[+] Keyboard Updated to V.3")
+    print("[!] v.3: Layout Full 27 tombol")
+    print("[!] WARNING: Tombol paling kanan bawah = rm -rf")
     print("[!] Jalankan: termux-reload-settings")
+   
 
 if __name__ == "__main__":
     install()
